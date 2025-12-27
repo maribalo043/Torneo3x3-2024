7666f3e315c489e7241a78abbc35c6899ff11fdf
maribalo <133330755+maribalo043@users.noreply.github.com>
2025-12-27 13:57:23 +0100

    Update 3PromesasClasificaciones.js

--- js/3PromesasClasificaciones.js
+++ js/3PromesasClasificaciones.js
@@ -213,7 +213,7 @@ var partidoA_sab4 = new Partido(Carniceras, Cantabrucos, '4-7', "13:30-14:00", "
 var partidoB_sab1 = new Partido(Colorinchis, Picapiedra, '5-2', "10:30-11:00", "Pista Sara Roces");
 var partidoB_sab2 = new Partido(Devasting, Celtic, '8-4', "10:30-11:00", "Pista Marta Piquero");
 var partidoB_sab3 = new Partido(Vascos, Calamares, '3-3', "11:00-11:30", "Pista Sara Roces");
-var partidoB_sab4 = new Partido(Celtic, Colorinchis, null, "12:30-13:00", "Pista Patinalon");
+var partidoB_sab4 = new Partido(Celtic, Colorinchis, '3-8', "12:30-13:00", "Pista Patinalon");
 var partidoB_sab5 = new Partido(Picapiedra, Vascos, null, "13:30-14:00", "Pista Patinalon");
 
 /*Finales DOMINGO 29 Diciembre--------------------------------------------------------------------------------------*/
