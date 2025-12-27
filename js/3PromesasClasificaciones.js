0aa230782e4108c9e899517cc9e36008afb7a54b
maribalo <133330755+maribalo043@users.noreply.github.com>
2025-12-27 11:00:53 +0100

    Add scores to Partido instances for Grupo B

--- js/3PromesasClasificaciones.js
+++ js/3PromesasClasificaciones.js
@@ -210,8 +210,8 @@ var partidoA_sab3 = new Partido(Patin, Bartolos, null, "13:30-14:00", "Pista Pat
 var partidoA_sab4 = new Partido(Carniceras, Cantabrucos, null, "13:30-14:00", "Pista Patinalon");
 
 // Partidos Grupo B - Sábado
-var partidoB_sab1 = new Partido(Colorinchis, Picapiedra, null, "10:30-11:00", "Pista Sara Roces");
-var partidoB_sab2 = new Partido(Devasting, Celtic, null, "10:30-11:00", "Pista Marta Piquero");
+var partidoB_sab1 = new Partido(Colorinchis, Picapiedra, '5-2', "10:30-11:00", "Pista Sara Roces");
+var partidoB_sab2 = new Partido(Devasting, Celtic, '8-4', "10:30-11:00", "Pista Marta Piquero");
 var partidoB_sab3 = new Partido(Vascos, Calamares, null, "11:00-11:30", "Pista Sara Roces");
 var partidoB_sab4 = new Partido(Celtic, Colorinchis, null, "12:30-13:00", "Pista Patinalon");
 var partidoB_sab5 = new Partido(Picapiedra, Vascos, null, "13:30-14:00", "Pista Patinalon");
