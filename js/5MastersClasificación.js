82ab3fce73bcc1b4854430fe3e116205b2280294
maribalo <133330755+maribalo043@users.noreply.github.com>
2025-12-27 15:16:03 +0100

    Update 5MastersClasificación.js

--- js/5MastersClasificación.js
+++ js/5MastersClasificación.js
@@ -196,9 +196,9 @@ var partidoB17 = new Partido(Rusticos, Black, '10-4', '20:30-21:00', 'Pista Sara
 /*GRUPO A--------------------------------------------------------------------------------------*/
 var partidoA21 = new Partido(Traviesos, Parrales, '4-6', '12:00-12:30', 'Pista Sara Roces');
 var partidoA22 = new Partido(Ladys, Failteam, '2-4', '12:00-12:30', 'Pista Marta Piquero');
-var partidoA23 = new Partido(ENRR, Failteam, null, '14:00-14:30', 'Pista Pista Patinalon');
+var partidoA23 = new Partido(ENRR, Failteam, '3-7', '14:00-14:30', 'Pista Pista Patinalon');
 var partidoA24 = new Partido(Traviesos, Getxokolate, '8-4', '14:00-14:30', 'Pista Marta Piquero');
-var partidoA25 = new Partido(Parrales, Ladys, null, '14:00-14:30', 'Sergio Villar');
+var partidoA25 = new Partido(Parrales, Ladys, '4-4', '14:00-14:30', 'Sergio Villar');
 
 /*GRUPO B--------------------------------------------------------------------------------------*/
 var partidoB21 = new Partido(Getxokolate, ENRR, '4-4', '11:30-12:00', 'Pista Sergio Villar');
