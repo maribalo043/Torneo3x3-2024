b82a9c0b7cc3144c72d39fc6550c244ed120f925
maribalo <133330755+maribalo043@users.noreply.github.com>
2025-12-28 13:52:45 +0100

    Update 5MastersClasificación.js

--- js/5MastersClasificación.js
+++ js/5MastersClasificación.js
@@ -213,8 +213,8 @@ var Cuarto3 = new PartidoFinal(Parrales,Rusticos,'5-4',"Sábado Cuarto 3: 21:00-
 var Cuarto4 = new PartidoFinal(Resaca,Ladys,'3-1',"Sábado Cuarto 4: 21:00-21:30","Pista Marta Piquero");
 var Semi1 = new PartidoFinal(Failteam,Merli,'4-5',"Semi 1: 11:00-11:30","Pista Sara Roces"); 
 var Semi2 = new PartidoFinal(Parrales,Resaca,'1-4',"Semi 2: 11:00-11:30","Pista Marta Piquero");
-var TercerCuarto = new PartidoFinal(Failteam,Parrales,null," TercerCuarto: 13:30-14:00","Pista Sara Roces");
-var Final = new PartidoFinal(Merli,Resaca,null,"Final: 13:30-14:00","Pista Marta Piquero");
+var TercerCuarto = new PartidoFinal(Failteam,Parrales,'2-3'," TercerCuarto: 13:30-14:00","Pista Sara Roces");
+var Final = new PartidoFinal(Merli,Resaca,'6-1',"Final: 13:30-14:00","Pista Marta Piquero");
 
 var ordenGrupo1 = [Parrales,ENRR,Failteam,Ladys,Traviesos,Getxokolate];
 var ordenGrupo2 = [Rusticos,Purys,Resaca,Black,Merli];
